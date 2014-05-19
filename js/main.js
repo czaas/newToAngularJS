cac9cdf5b961d1ca5b85d54167f9d300a80f63cc
czaas <cameronzaas@gmail.com>
2014-05-19 01:12:27 -0700

    Seperate js file for easy editing

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function SimpleController($scope){
+	$scope.people = [
+					{name:'Cameron Zaas', city:'Nipomo'}, 
+					{name:'Emily Zaas', city:'AG'}, 
+					{name:'Liam Zaas', city:'Pismo'}, 
+					{name:'Dylan Zaas', city:'San Luis Obispo'},
+					{name: 'Corey Zaas', city: 'Santa Maria'}
+				];
+}
